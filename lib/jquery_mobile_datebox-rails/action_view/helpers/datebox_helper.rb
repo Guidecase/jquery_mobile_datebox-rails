c7a3409510757c39307ae763912f59e90142a87b
Travis Dunn <dunn.travis@gmail.com>
2012-09-24 14:52:55 +0200

    add form helper

--- lib/jquery_mobile_datebox-rails/action_view/helpers/datebox_helper.rb
+++ lib/jquery_mobile_datebox-rails/action_view/helpers/datebox_helper.rb
@@ -5,6 +5,7 @@ module ActionView
     
       def datebox_tag(name, value, mode, cal_options = {}, options = {})
         options['data'] ||= {}
+        cal_options['mode'] = mode
         options['data']['options'] = cal_options.to_json
         options['data']['role'] = 'datebox'
         text_field_tag name, value, options
--- lib/jquery_mobile_datebox-rails/action_view/helpers/form_helper.rb
+++ lib/jquery_mobile_datebox-rails/action_view/helpers/form_helper.rb
@@ -1,15 +1,20 @@
 module ActionView
   module Helpers
     module FormHelper
-      def datebox(object_name, method, mode, options = {}, html_options = {})
-        Tags::DateboxField.new(object_name, method, self, options, html_options).render
-      end    	
+      def datebox(object_name, method, mode, cal_options = {}, options = {})
+        options['data'] ||= {}
+        cal_options['mode'] = mode
+        options['data']['options'] = cal_options.to_json
+        options['data']['role'] = 'datebox'
+        
+        text_field object_name, method, options
+      end   
     end
 
-  	class FormBuilder
-      def datebox(method, mode = 'calbox', options = {}, html_options = {})
-        @template.datebox_field(@object_name, method, mode, objectify_options(options), html_options)
-      end  	  
-	  end  	
+    class FormBuilder
+      def datebox(method, mode = 'calbox', cal_options = {}, options = {})
+        @template.datebox(@object_name, method, mode, cal_options, objectify_options(options))
+      end     
+    end   
   end
 end
